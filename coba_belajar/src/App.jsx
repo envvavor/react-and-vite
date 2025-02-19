de531ca9a46df6120c1e5d7d419d8a60554d159e
envvavor <arya.zusu@gmail.com>
2025-02-19 14:55:24 +0700

    second

--- coba_belajar/src/App.jsx
+++ coba_belajar/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
@@ -18,8 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          you are an ass in {count} different country
+        <button onClick={() => setCount((count) => count * 2)}>
+          count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
